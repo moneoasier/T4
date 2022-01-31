81cf16fa58229d75208a4f70db780742e578111e
moneoasier <moneo.asier@uni.eus>
2022-01-31 13:21:27 +0100

    report+ design

---
+++ ErronkaOndo/ErronkaOndo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ErronkaOndo.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ErronkaOndo.Areas.Identity.IdentityHostingStartup))]
+namespace ErronkaOndo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ ErronkaOndo/ErronkaOndo/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErronkaOndo.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ReportController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Inprimatu()
+        {
+            string mimtype = "";
+            int extension = 1;
+            var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("rp1", "WineShop Denda online");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimtype);
+            return File(result.MainStream, "application/pdf");
+        }
+       
+    }
+}
